4bb7149f3efcc19cf21cec35a372f48190cb5f34
Joanna Pham <jopham90@gmail.com>
2020-09-21 15:46:20 -0700

    revised changes

--- components/components/app.js
+++ components/components/app.js
@@ -1,14 +1,14 @@
 class App {
+  constructor(gradeTable) {
+    this.gradeTable = gradeTable;
+    this.handleGetGradesError = this.handleGetGradesError.bind(this);
+    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+  }
   handleGetGradesError(error) {
     console.log(error)
   }
   handleGetGradesSuccess(grades) {
     this.gradeTable.updateGrades(grades) //pass its grades parameter to the updateGrades() method of the gradeTable property of the this object (instead of logging grades to the console).
-  }
-  constructor(gradeTable) {
-    this.gradeTable = gradeTable;
-    this.handleGetGradesError = this.handleGetGradesError.bind(this);
-    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
   }
   getGrades() {
     $.ajax({
